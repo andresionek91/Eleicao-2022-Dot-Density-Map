c4218d6d1817dc02f1b0929a5bb68092b9e6108c
Andre Sionek <andresionek91@gmail.com>
2022-11-06 03:11:59 +0000

    fix: fix reduce memory

--- src/data_processing/stack.py
+++ src/data_processing/stack.py
@@ -43,7 +43,7 @@ class DataProcessingStack(cdk.Stack):
             entry="src/data_processing/functions/enrich_addresses",
             environment={"delivery_stream_name": self.delivery_stream.delivery_stream_name},
             timeout=cdk.Duration.minutes(amount=15),
-            memory_size=256,
+            memory_size=128,
             dead_letter_queue_enabled=True,
         )
 
