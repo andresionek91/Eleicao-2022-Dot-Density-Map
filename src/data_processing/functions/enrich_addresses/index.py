224d79f3f1fcfbd1338ad92d6e5ac292693da27b
Andre Sionek <andresionek91@gmail.com>
2022-11-06 01:30:37 +0000

    feat: create infra for enrichment

---
+++ src/data_processing/functions/enrich_addresses/index.py
@@ -0,0 +1,99 @@
+import json
+
+from typing import List
+
+import backoff
+import boto3
+import requests
+
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools import Tracer
+from aws_lambda_powertools.utilities.parser import BaseModel
+from aws_lambda_powertools.utilities.parser import event_parser
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+
+logger = Logger()
+tracer = Tracer()
+
+firehose = boto3.client("firehose")
+
+
+class Secao(BaseModel):
+    """Secao eleitoral"""
+
+    nr_zone: str
+    nr_secao: str
+    nome_local_votacao: str
+    endereco_local_votacao: str
+    municipio_local_votacao: str
+    uf_local_votacao: str
+
+
+class Secoes(BaseModel):
+    """Conjunto de secoes"""
+
+    items: List[Secao]
+
+
+@backoff.on_exception(backoff.expo, requests.RequestException, max_time=20)
+def get_osm_data(query: str) -> dict:
+    """Make a request to Photon OSM"""
+    response = requests.get(f"https://photon.komoot.io/api/?q={query}")
+    return response.json()
+
+
+def parse_osm_data(data: dict) -> dict:
+    """Parse OSM Response"""
+    return {
+        "lat": data["features"][0]["geometry"]["coordinates"][0],
+        "long": data["features"][0]["geometry"]["coordinates"][1],
+        "postcode": data["features"][0]["properties"].get("postcode", None),
+        "street": data["features"][0]["properties"].get("street", None),
+        "district": data["features"][0]["properties"].get("district", None),
+        "city": data["features"][0]["properties"].get("city", None),
+        "countrycode": data["features"][0]["properties"].get("countrycode", None),
+    }
+
+
+@logger.inject_lambda_context
+@tracer.capture_lambda_handler
+@event_parser(model=Secoes)
+def handler(event: Secoes, context: LambdaContext) -> None:
+    """Get a chunk of records and enrich it using https://photon.komoot.io/api/?q={query} and send to kinesis firehose"""
+    enriched_items = ""
+
+    for item in event.items:
+        try:
+            query = f"{item.endereco_local_votacao} {item.municipio_local_votacao} {item.uf_local_votacao}"
+            enriched = parse_osm_data(get_osm_data(query))
+            enriched = {**item.dict(), **enriched, "enrichment quality": 3}
+        except (KeyError, IndexError):
+            enriched = None
+
+        if not enriched:
+            try:
+                query = f"{item.endereco_local_votacao.split(',')[0]}, {item.municipio_local_votacao}, {item.uf_local_votacao}"
+                enriched = parse_osm_data(get_osm_data(query))
+                enriched = {**item.dict(), **enriched, "enrichment quality": 2}
+            except (KeyError, IndexError):
+                enriched = None
+
+        if not enriched:
+            try:
+                query = f"{item.municipio_local_votacao}, {item.uf_local_votacao}, BRASIL"
+                enriched = parse_osm_data(get_osm_data(query))
+                enriched = {**item.dict(), **enriched, "enrichment quality": 1}
+            except (KeyError, IndexError):
+                return
+
+        json_enriched = json.dumps(enriched) + "\n"
+
+        enriched_items += json_enriched
+
+    firehose.put_record_batch(
+        DeliveryStreamName="string",
+        Records=[
+            {"Data": enriched_items.encode("latin-1")},
+        ],
+    )
--- src/data_processing/stack.py
+++ src/data_processing/stack.py
@@ -1,5 +1,10 @@
 import aws_cdk as cdk
 
+from aws_cdk import aws_kinesisfirehose_alpha as firehose
+from aws_cdk import aws_kinesisfirehose_destinations_alpha as destinations
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_lambda_python_alpha as lambda_python
+from aws_cdk import aws_s3 as s3
 from constructs import Construct
 
 from src.data_processing.config import Config
@@ -11,6 +16,34 @@ class DataProcessingStack(cdk.Stack):
     def __init__(self, scope: Construct, id: str, config: Config, **kwargs) -> None:
         description = "Data Processing Eleicoes 2022"
         super().__init__(scope, id, description=description, **kwargs)
+
+        self.bucket = s3.Bucket(scope=self, id="EnrichmentDataBucket", bucket_name="sionek-eleicoes-2022-enrichment")
+
+        destination = destinations.S3Bucket(
+            bucket=self.bucket,
+            buffering_interval=cdk.Duration.minutes(amount=10),
+            buffering_size=cdk.Size.mebibytes(amount=20),
+            compression=destinations.Compression.GZIP,
+            data_output_prefix="secoes_enriched_addresses/",
+            logging=True,
+        )
+
+        self.delivery_stream = firehose.DeliveryStream(
+            scope=self,
+            id="EnrichAddressesFirehose",
+            destinations=[destination],
+        )
+
+        self.function = lambda_python.PythonFunction(
+            scope=self,
+            id="EnrichAddressesFunction",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="handler",
+            entry="src/data_processing/functions/enrich_addresses",
+            environment={"delivery_stream_name": self.delivery_stream.delivery_stream_name},
+        )
+
+        self.delivery_stream.grant_put_records(self.function)
 
         # Add tags to everything in this stack
         cdk.Tags.of(self).add(key="stack", value=self.stack_name)
